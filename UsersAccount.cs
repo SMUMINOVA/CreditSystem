97ad696b01b2e671d1521699f63f50333c87d57b
Sobira <sobira204@gmail.com>
2020-04-21 19:35:24 +0500

    create classes UsersAccount and UsersForm

---
+++ UsersAccount.cs
@@ -0,0 +1,11 @@
+namespace FirstProject
+{
+    public class UsersAccount
+    {
+        public EntryToSystem Entry{get; set;}
+        public string Name{get;set;}
+        public string Surname{get; set;}
+        public string DocumentNo{get; set;}
+        public UsersForm Form{get;set;}
+    }
+}
--- UsersForm.cs
+++ UsersForm.cs
@@ -2,6 +2,13 @@ namespace FirstProject
 {
     public class UsersForm
     {
-        
+        public string Gender{get; set;}
+        public string FamilyStatus{get; set;}
+        public int Age{get; set;}
+        public string CityZone{get; set;}
+        public int CreditsHistory{get; set;}
+        public int OutstandingCredits{get; set;}
+        public string GoalOfCredit{get; set;}
+        public int PeriodOfCredit{get; set;}
     }
 }
