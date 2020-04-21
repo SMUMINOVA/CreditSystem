7428d4a3baf0da9c9b9959c6aa9d4635105bb59c
Sobira <sobira204@gmail.com>
2020-04-21 17:43:16 +0500

    add conection with sql

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace newProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+            SqlConnection scon = new SqlConnection(conString);
+            scon.Open();
+            if(scon.State == ConnectionState.Open)
+                System.Console.WriteLine("Connected successfully");
+            else 
+                System.Console.WriteLine("Oooops, some problems");
+        }
+    }
+}
